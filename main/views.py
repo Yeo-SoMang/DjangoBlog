87bc60ad23f45fe181769972718ac3e9b603bae2
Yeo-SoMang <77438122+Yeo-SoMang@users.noreply.github.com>
2021-02-07 20:07:07 +0900

    Initial commit

---
+++ main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Post
+
+def index(request):
+    postAll = Post.objects.all()
+    return render(request, 'main/index.html',{'postAll':postAll})
+
+def postdetail(request,pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'main/postdetail.html', {'post': post})
